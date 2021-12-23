51e79da6e3007967b2f7632499fc17a0dc3ccc26
Pavel Trushnikov <oracal@mail.ru>
2021-12-23 22:20:31 +0300

    Выполнение задания 20 урока

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import timer from './modules/timer';
+import menu from './modules/menu';
 
-timer('23 december 2021');
+timer('25 december 2021');
+menu();
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,3 +1,44 @@
 const menu = () => {
+    const menuBtn = document.querySelector('.menu');
+    const menu = document.querySelector('menu');
+    const closeBtn = menu.querySelector('.close-btn');
+    const menuItems = menu.querySelectorAll('ul>li>a');
+    const activeMenu = () => menu.classList.toggle('active-menu');
+    const popup = document.querySelector('.popup');
+    const popupClose = popup.querySelector('.popup-close');
+    const activePopupBtn = document.querySelectorAll('.service .popup-btn');
     
+    menuBtn.addEventListener('click', activeMenu);
+    closeBtn.addEventListener('click', activeMenu);
+    menuItems.forEach( item => item.addEventListener('click', activeMenu));
+    activePopupBtn.forEach( item => item.addEventListener('click', () => {
+        let stepAnimate = 0;
+        let animatePopup;
+
+        popup.style.opacity = 0;
+        popup.style.display = 'block';
+        if(!(document.documentElement.clientWidth < 768)) {
+            let animatePopupRun = () => {
+                stepAnimate += 0.03;
+                animatePopup = requestAnimationFrame(animatePopupRun);
+                if(stepAnimate < 1) {
+                    popup.style.opacity = stepAnimate;
+                } else {
+                    cancelAnimationFrame(animatePopup);
+                }
+            };
+            animatePopupRun();
+        } else {
+            popup.style.opacity = 1;
+        }
+        
+        console.dir(document.documentElement.clientWidth);
+    }));
+    popupClose.addEventListener('click', () => {
+        popup.style.display = 'none';
+    });
+
+    console.log(activePopupBtn);
 };
+
+export default menu;
